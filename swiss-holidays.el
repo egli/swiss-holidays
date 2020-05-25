b5f7d02ab4bf27270bc4a141c2bf62dbc60c6804
Christian Egli <christian.egli@sbs.ch>
2020-05-25 15:41:51 +0200

    Update the commentary

--- swiss-holidays.el
+++ swiss-holidays.el
@@ -30,10 +30,20 @@
 ;;
 ;;  (setq holiday-other-holidays swiss-holidays)
 ;;
-;; If you'd like to add regional holidays, use the following:
+;; If you'd like to add regional holidays, pick additional holidays
+;; from `swiss-holidays-catholic', `swiss-holidays-epiphany',
+;; `swiss-holidays-st-joseph-day' or `swiss-holidays-labour-day'.
+;;
+;; For the canton of Zürich for example you'd use the following:
 ;;
 ;;  (setq holiday-other-holidays
-;;        (append swiss-holidays swiss-holidays-catholic))
+;;        (append swiss-holidays swiss-holidays-labour-day))
+;;
+;; For the canton of Schwyz you could use the following:
+;;
+;;  (setq holiday-other-holidays
+;;        (append swiss-holidays swiss-holidays-catholic
+;;                swiss-holidays-epiphany swiss-holidays-st-joseph-day))
 ;;
 ;; The code was inspired by russian-holidays.el and
 ;; https://www.emacswiki.org/emacs/CalendarLocalization
